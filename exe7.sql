c465501c7292f95260f1e1409afb598551a240f8
taispin <tais141@gmail.com>
2018-04-13 19:55:39 -0300

    7.1a

---
+++ exe7.sql
@@ -0,0 +1,15 @@
+﻿/* Nome: taís Pinheiro NUSp: 7580421*/
+/* MAC0439 - Laboratorio de Banco de Dados*/
+/* Exercicio 7 */
+
+set search_path to mac439_aula12;
+
+/* Exercicio 7.1*/
+
+/*a- Usando apenas as relações Classes e Navios, produza todas as informações disponíveis sobre
+navios, incluindo as informações disponíveis na relação Classes. Você deve produzir informações
+sobre uma classe mesmo se não existir nenhum navio da classe na relação Navios.*/
+
+
+select * 
+from navios natural full outer join classes;
