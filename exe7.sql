f7f42e66fb6c26430c2aeaaf52971b13d256c827
taispin <tais141@gmail.com>
2018-04-17 18:37:01 -0300

    juncoes

--- exe7.sql
+++ exe7.sql
@@ -1,4 +1,4 @@
-﻿/* Nome: taís Pinheiro NUSp: 7580421*/
+﻿﻿/* Nome: taís Pinheiro NUSp: 7580421*/
 /* MAC0439 - Laboratorio de Banco de Dados*/
 /* Exercicio 7 */
 
@@ -14,19 +14,16 @@ sobre uma classe mesmo se não existir nenhum navio da classe na relação Navio
 select * 
 from navios natural full outer join classes;
 
-/* Essa pergunta parace Confusa!*/
-/* Mandei pergunta no paca*/
 /*b- Liste todos os dados disponíveis (i.e., nome e data) de todas as batalhas que aparecem no BD.
 Uma batalha deve aparecer na resposta mesmo quando não é possível saber em que data ela
 ocorreu.*/
 
-select nome, data 
+select DISTINCT nome,data
 from batalhas left outer join resultados on (nome = batalha);
 
 /*c- Forneça informações sobre todos os produtos do BD (PCs, laptops e impressoras), incluindo o
 seu fabricante se disponível, e quaisquer outras informações sobre o produto que sejam relevantes
 (ou seja, as informações encontradas na relação para o tipo de produto em questão).*/
 
-select fabricante,produto.modelo, produto.tipo as tipo_produto, velocidade,ram,hd,preco,cd,colorida,todos.tipo,tela
-from produto,(pc natural full outer join (impressora natural full outer join laptop)) AS todos
-where todos.modelo = produto.modelo;
+select *
+from produto left outer join (pc natural full outer join (impressora natural full outer join laptop)) AS todos on (todos.modelo = produto.modelo)
