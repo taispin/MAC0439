f33e89b7b8821ee20b90c959283ff146508ace1c
Tais Pin <tais141@gmail.com>
2018-04-23 23:52:20 -0300

    exercicio8-a

---
+++ Tais-ex8.sql
@@ -0,0 +1,10 @@
+﻿﻿﻿/* Nome: taís Pinheiro NUSp: 7580421*/
+/* MAC0439 - Laboratorio de Banco de Dados*/
+/* Exercicio 8 */
+
+
+/*a) Para cada ator, devolva o seu ano de estreia em filmes.*/
+
+MATCH (person:Person)
+ -[:ACTED_IN]->(movie:Movie)
+RETURN person.name, min((movie.released));
