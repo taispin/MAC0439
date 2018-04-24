d02e2100d46aaaf2145a113df846f2ab0176433b
taispin <tais141@gmail.com>
2018-04-24 17:23:01 -0300

    b

--- Tais-ex8.sql
+++ Tais-ex8.sql
@@ -1,4 +1,4 @@
-﻿﻿﻿/* Nome: taís Pinheiro NUSp: 7580421*/
+*﻿﻿﻿/* Nome: taís Pinheiro NUSp: 7580421*/
 /* MAC0439 - Laboratorio de Banco de Dados*/
 /* Exercicio 8 */
 
@@ -8,3 +8,9 @@
 MATCH (person:Person)
  -[:ACTED_IN]->(movie:Movie)
 RETURN person.name, min((movie.released));
+
+/*b) Para cada pessoa no BD, devolva a sua lista de conhecidos (ou seja, pessoas que trabalharam em
+filmes em que ela trabalhou)*/
+
+MATCH (p:Person)-[r1]->(movie:Movie)<-[r2]-(q:Person)
+RETURN q.name AS Pessoa,movie.title AS Filme, p.name AS Conhecido
