0732710ebce754939ec38ca8956a925cd3832c3b
taispin <tais141@gmail.com>
2018-04-24 20:17:18 -0300

    d

--- Tais-ex8.sql
+++ Tais-ex8.sql
@@ -14,3 +14,24 @@ filmes em que ela trabalhou)*/
 
 MATCH (p:Person)-[r1]->(movie:Movie)<-[r2]-(q:Person)
 RETURN q.name AS Pessoa,movie.title AS Filme, p.name AS Conhecido
+
+/*c) Mostre o nome de cada diretor que já dirigiu dois filmes em sequência sem atuar em nenhum
+outro filme lançado entre eles.*/
+
+MATCH (diretor:Person)-[:DIRECTED]->(filme1:Movie),
+(diretor)-[:DIRECTED]->(filme2:Movie)
+MATCH(filme:Movie)
+WHERE filme1.title <> filme2.title
+AND filme1.released <= filme2.released
+AND filme.released >= filme1.released
+AND filme.released <= filme2.released
+AND  NOT exists( (diretor)-[:ACTED_IN]->(filme) )
+RETURN DISTINCT diretor.name;
+
+/*d) O Tom Hanks está procurando atores para atuar em um novo filme que ele vai dirigir.
+Recomende possíveis atores para esse novo filme dele.*/
+/*Recomendação: Atores que já atuaram em filmes que ele esteve ligado de alguma forma*/
+
+MATCH (recomendado:Person)-[r]->(filme:Movie)<-[s]-(diretor:Person)
+WHERE diretor.name="Tom Hanks"
+RETURN DISTINCT recomendado.name;
