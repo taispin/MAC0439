1d6ef074fcd83d16a83db9e90b1f9d01d93c8444
Taís Pinheiro <tais141@gmail.com>
2018-06-20 12:00:06 -0300

    item 2

--- Tais_ex16.sql
+++ Tais_ex16.sql
@@ -1,29 +1,9 @@
 ﻿/* Tais Pinheiro nusp: 7580421 */
 /* Lista de Exercicios 16 */
 
+----------------------------------------------------------
 -- zera tudo.
 DROP SCHEMA mac439_exercicio16 cascade;  
-
-CREATE SCHEMA mac439_exercicio16;
-
-SET SEARCH_PATH to 'mac439_exercicio16';
-
--- Exercício 01
-
-CREATE TABLE Empregados (
-
-	nome VARCHAR(50) PRIMARY KEY,
-	dept  VARCHAR(50) NOT NULL,
-	salario DECIMAL(10,2) NOT NULL
-);
-
-CREATE VIEW NomesDosEmpregados AS
-SELECT nome FROM Empregados; 
-
-CREATE VIEW InfosDosDepts(dept,salariomedio) AS
-SELECT dept, avg(salario)
-FROM Empregados
-GROUP BY dept; 
 
 -- teste
 insert into Empregados values('Tais','bcc',5000);
@@ -41,6 +21,33 @@ insert into Empregados values('Mauricio4','Informática',8000);
 select * from NomesDosEmpregados;
 select * from InfosDosDepts;
 
+-- item 02:
+select * from InfosDosDepts_preProducao_informatica;
+--------------------------------------------
+
+CREATE SCHEMA mac439_exercicio16;
+
+SET SEARCH_PATH to 'mac439_exercicio16';
+
+-- Exercício 01
+/* Escreva comandos SQL para a criação das visões NomesDosEmpregados and InfosDosDepts.  */
+
+CREATE TABLE Empregados (
+
+	nome VARCHAR(50) PRIMARY KEY,
+	dept  VARCHAR(50) NOT NULL,
+	salario DECIMAL(10,2) NOT NULL
+);
+
+CREATE VIEW NomesDosEmpregados AS
+SELECT nome FROM Empregados; 
+
+CREATE VIEW InfosDosDepts(dept,salariomedio) AS
+SELECT dept, avg(salario)
+FROM Empregados
+GROUP BY dept; 
+
+
 -- Exercício 02
 /* Quais privilégios devem ser dados a um usuário que precisa saber apenas os salários médios para os departamentos 
 “Pré-produção” e “Informática”? Escreva o comando correspondente.*/
@@ -50,8 +57,6 @@ CREATE VIEW InfosDosDepts_preProducao_informatica AS
 SELECT * 
 FROM InfosDosDepts
 WHERE dept = 'Pré-produção' OR dept = 'Informática';
-
-select * from InfosDosDepts_preProducao_informatica;
 
 -- Com a nova visão criada, concedemos os privilégios
 GRANT USAGE ON SCHEMA mac439_exercicio16 TO userglobal1;
